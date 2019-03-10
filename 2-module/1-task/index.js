cab29d5f8df08c0552f913651ffe0ebc878b3c4a
Melnik <melnik@softbalance.ru>
2019-03-10 15:08:48 +0300

    second module, firt task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -4,4 +4,17 @@
  * @returns {Object}
  */
 function clone (obj) {
+  const newObj = {};
+
+  for (let prop in obj) {
+
+    if ((typeof obj[prop] === 'object') && !!obj[prop]) {
+      newObj[prop] = clone(obj[prop]);
+    } else {
+      newObj[prop] = obj[prop];
+    }
+  }
+
+
+  return newObj;
 }
