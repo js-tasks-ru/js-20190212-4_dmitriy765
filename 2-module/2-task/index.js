3855657564ba84f37eea3b55e51849b99dbd3b8b
Melnik <melnik@softbalance.ru>
2019-03-23 19:06:31 +0300

    task 2 module 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -4,5 +4,37 @@
  * @param {*} value - значение, которе ищем
  * @returns {Object}
  */
-function find (obj, value) {
+let arr = [];
+
+function findRecourseve(obj, value, path){
+
+  for (let item in obj) {
+    if (typeof obj[item] === "object") {
+      let thisPath = !!path ? path+'.'+item : item;
+
+      findRecourseve(obj[item], value, thisPath);
+    } else {
+      if (obj[item] === value) {
+        let thisPath = !!path ? path+'.'+item : item;
+
+        arr.push(thisPath);
+      }
+    }
+  }
+
+  // return arr;
+}
+
+function find(obj, value) {
+
+  findRecourseve(obj, value);
+
+  const tmpArr = arr;
+  arr = [];
+
+  if (tmpArr.length !== 0) {
+    return tmpArr.length === 1 ? tmpArr[0] : tmpArr;
+  } else {
+    return null;
+  }
 }
