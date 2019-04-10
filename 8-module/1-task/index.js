bff7080bac3b438955ff666b53754bb24b56aa91
Melnik <melnik@softbalance.ru>
2019-04-10 14:52:57 +0300

    8 module 1 task

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -20,13 +20,63 @@ function SortableTable(items) {
     /**
      * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
      */
-    this.el = document.createElement('table');
+    const _ = this;
+    const tHead = document.createElement('thead');
+    const headRow = document.createElement('tr');
+    _.el = document.createElement('table');
+    _.tBody = document.createElement('tbody');
+    _.tBodyRows = [];
+
+    items.forEach(function (item, i) {
+
+      const bodyRow = document.createElement('tr');
+
+      for (let key in item){
+        if (i === 0){
+          let headCol = document.createElement('td');
+          headCol.innerText = key;
+          headRow.appendChild(headCol);
+        }
+
+        const bodyCol = document.createElement('td');
+        bodyCol.innerText = item[key];
+        bodyRow.appendChild(bodyCol);
+      }
+
+      _.tBody.appendChild(bodyRow);
+      _.tBodyRows.push(bodyRow);
+
+    });
+
+    tHead.appendChild(headRow);
+    _.el.appendChild(tHead);
+    _.el.appendChild(_.tBody);
+
 
     /**
      * Метод выполняет сортировку таблицы
      * @param {number} column - номер колонки, по которой нужно выполнить сортировку (отсчет начинается от 0)
      * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
      */
-    this.sort = function (column, desc = false) { };
-}
+    _.sort = function (column, desc = false) {
+      _.tBodyRows.sort(function (current, next) {
+        if ( parseInt(current.cells[column].innerHTML) ){
+          if (desc) {
+            return next.cells[column].innerHTML - current.cells[column].innerHTML;
+          } else {
+            return current.cells[column].innerHTML - next.cells[column].innerHTML;
+          }
+        } else {
+          if (desc) {
+            return (next.cells[column].innerHTML > current.cells[column].innerHTML) ? 1 : -1;
+          } else {
+            return (next.cells[column].innerHTML > current.cells[column].innerHTML) ? -1 : 1;
+          }
+        }
+      });
 
+      for (let i = 0; i < _.tBodyRows.length; i++) {
+        _.tBody.appendChild(_.tBodyRows[i])
+      }
+    };
+}
